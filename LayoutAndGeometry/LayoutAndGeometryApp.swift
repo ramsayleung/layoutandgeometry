9ac3a6a387ed0529bd98d995ba308bd1119694a6
Ramsay Leung <ramsayleung@gmail.com>
2024-04-01 18:31:59 -0700

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by ramsayleung on 2024-04-01.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
